a87f7fdce12848eb980a0d73302a734148e539f0
liyuanshuo <1405024239@st.nuc.edu.cn>
2018-03-09 17:23:18 +0800

    initial commit

---
+++ 4_1SpecialMultip/cmake-build-debug/CMakeFiles/4_1SpecialMultip.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/4_1SpecialMultip.dir/main.cpp.obj"
+  "4_1SpecialMultip.pdb"
+  "4_1SpecialMultip.exe"
+  "4_1SpecialMultip.exe.manifest"
+  "lib4_1SpecialMultip.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4_1SpecialMultip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
