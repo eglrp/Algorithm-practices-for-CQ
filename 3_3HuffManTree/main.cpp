14e530ac0ed6b6a4bbb4c6ddfced69807c5e51d8
liyuanshuo <1405024239@st.nuc.edu.cn>
2018-03-09 13:39:22 +0800

    initial commit

--- 3_3HuffManTree/main.cpp
+++ 3_3HuffManTree/main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
+priority_queue<int, vector<int>, greater<int> > Q;
+
+
 int main()
 {
+
+    int n;
+    while ( cin>>n )
+    {
+        while ( Q.empty() == false )
+            Q.pop();
+        for (int i = 1; i <= n ; ++i)
+        {
+            int x;
+            cin>>x;
+            Q.push(x);
+        }
+        int ans = 0;
+        while ( Q.size() > 1 )
+        {
+            int a = Q.top();
+            Q.pop();
+            int b = Q.top();
+            Q.pop();
+            ans += a + b;
+            Q.push(a+b);
+        }
+        cout<<ans<<endl;
+    }
 
     //std::cout << "Hello, World!" << std::endl;
 
