c18a9408bd33372fb48eb0ab95d801e3e690644b
liyuanshuo <1405024239@st.nuc.edu.cn>
2018-03-09 12:53:48 +0800

    initial commit

--- 3_2SimpCalculate/main.cpp
+++ 3_2SimpCalculate/main.cpp
@@ -1,7 +1,67 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
+char str[200];
+int mat[][5] = {
+        1, 0, 0, 0, 0,
+        1, 0, 0, 0, 0,
+        1, 0, 0, 0, 0,
+        1, 1, 1, 0, 0,
+        1, 1, 1, 0, 0
+};
+
+stack<int> op;
+stack<double> in;
+
+void  get_op( bool &reto, int &retn, int &i )
+{
+    if( i==0 && op.empty()==true )
+    {
+        reto = true;
+        retn = 0;
+        return;
+    }
+    if( str[i] == 0 )
+    {
+        reto = true;
+        retn = 0;
+        return;
+    }
+    if( str[i] >= '0' && str[i] <= '9' )
+    {
+        reto = false;
+    } else
+    {
+        reto = true;
+        if( str[i] == '+' )
+        {
+            retn = 1;
+        }
+        else
+            if( str[i] == '-' )
+                retn = 2;
+            else
+                if( str[i] == '*' )
+                    retn = 3;
+                else
+                    if( str[i] == '/' )
+                        retn = 4;
+        i += 2;
+        return;
+    }
+
+    retn = 0;
+    for( ; str[i] != ' '&& str[i] != 0; i++ )
+    {
+        retn *= 10;
+        retn += str[i] - '0';
+    }
+    if ( str[i] == ' ' )
+        i++;
+    return;
+}
 
 int main()
 {
