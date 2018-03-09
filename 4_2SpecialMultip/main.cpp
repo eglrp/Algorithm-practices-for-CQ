ce93b3ad9ec9c72ec88e4a6664c4a31d340dda7b
liyuanshuo <1405024239@st.nuc.edu.cn>
2018-03-09 17:26:06 +0800

    initial commit

--- 4_2SpecialMultip/main.cpp
+++ 4_2SpecialMultip/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
+using namespace std;
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    char a[11], b[11];
+    while ( cin>>a>>b )
+    {
+        int ans = 0;
+        for (int i = 0; a[i] != 0 ; ++i)
+        {
+            for (int j = 0; b[j] != 0 ; ++j)
+            {
+                ans += ( a[i]-'0') * ( b[j] - '0' );
+            }
+        }
+        cout<<ans<<endl;
+    }
+
+    //std::cout << "Hello, World!" << std::endl;
     return 0;
 }
