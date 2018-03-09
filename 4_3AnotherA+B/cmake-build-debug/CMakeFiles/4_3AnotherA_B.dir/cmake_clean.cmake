7ee2d710f566d88b161e73a293d44c4245a87c22
liyuanshuo <1405024239@st.nuc.edu.cn>
2018-03-09 19:42:04 +0800

    initial commit

---
+++ 4_3AnotherA+B/cmake-build-debug/CMakeFiles/4_3AnotherA_B.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/4_3AnotherA_B.dir/main.cpp.obj"
+  "4_3AnotherA_B.pdb"
+  "4_3AnotherA_B.exe"
+  "4_3AnotherA_B.exe.manifest"
+  "lib4_3AnotherA_B.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4_3AnotherA_B.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
