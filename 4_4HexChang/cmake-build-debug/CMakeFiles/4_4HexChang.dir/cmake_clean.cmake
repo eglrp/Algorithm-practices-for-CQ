9f472e8aecdca64ccff91477c7fa0935c78f4641
liyuanshuo <1405024239@st.nuc.edu.cn>
2018-03-09 20:06:20 +0800

    initial commit

---
+++ 4_4HexChang/cmake-build-debug/CMakeFiles/4_4HexChang.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/4_4HexChang.dir/main.cpp.obj"
+  "4_4HexChang.pdb"
+  "4_4HexChang.exe"
+  "4_4HexChang.exe.manifest"
+  "lib4_4HexChang.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4_4HexChang.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
