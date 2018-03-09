37ba703cadb8ffd97668254a0fd921d0018ddc57
liyuanshuo <1405024239@st.nuc.edu.cn>
2018-03-09 20:03:50 +0800

    initial commit

--- 4_4HexChang/main.cpp
+++ 4_4HexChang/main.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include <cstring>
 
+
+using namespace std;
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+
+    int a, b;
+    char str[40];
+    while ( cin>>a>>str>>b )
+    {
+        int tmp = 0, x = 0;
+        int lenth = strlen(str), c = 1;
+        for (int i = lenth-1; i >=0 ; --i)
+        {
+            if( str[i] >= '0' && str[i] <= '9' )
+                x = str[i] - '0';
+            else
+                if( str[i] >= 'a' && str[i] <= 'z' )
+                    x = str[i] - 'a' + 10;
+                else
+                    x = str[i] - 'A' + 10;
+            tmp += x *c ;
+             c *= a;
+        }
+        char ans[40];
+        int size = 0;
+        do
+        {
+            x = tmp%b;
+            ans[size++] = (x<10) ? x+'0' : x-10+'A';
+            tmp /= b;
+        }while (tmp);
+
+        for (int j = size-1; j >= 0 ; --j)
+        {
+            cout<<ans[j];
+        }
+        cout<<endl;
+    }
+
+
+    //std::cout << "Hello, World!" << std::endl;
     return 0;
 }
