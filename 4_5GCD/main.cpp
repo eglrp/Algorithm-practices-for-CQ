89a36eb23079dbb0445b68ccfab4db47456b649f
liyuanshuo <1405024239@st.nuc.edu.cn>
2018-03-09 20:15:41 +0800

    initial commit

--- 4_5GCD/main.cpp
+++ 4_5GCD/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 
+
+using namespace std;
+
+int gcd1( int a, int b)
+{
+    if( b==0 )
+        return a;
+    else
+        return gcd1(b, a%b);
+}
+
+int gcd2( int a , int b)
+{
+    while( b!= 0)
+    {
+        int t = a % b;
+        a = b;
+        b = t;
+    }
+    return a;
+}
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+
+    int a, b;
+    while ( cin>>a>>b )
+        cout<<gcd1(a,b)<<" : "<<gcd2(a, b)<<endl;
+
+    //std::cout << "Hello, World!" << std::endl;
     return 0;
 }
