edf11aa7630dd5ac998b94fde4dfdb9e373b834b
liyuanshuo <1405024239@st.nuc.edu.cn>
2018-03-09 13:31:15 +0800

    initial commit

---
+++ 3_2SimpCalculate/cmake-build-debug/CMakeFiles/3_2SimpCalculate.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/3_2SimpCalculate.dir/main.cpp.obj"
+  "3_2SimpCalculate.pdb"
+  "3_2SimpCalculate.exe"
+  "3_2SimpCalculate.exe.manifest"
+  "lib3_2SimpCalculate.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_2SimpCalculate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
