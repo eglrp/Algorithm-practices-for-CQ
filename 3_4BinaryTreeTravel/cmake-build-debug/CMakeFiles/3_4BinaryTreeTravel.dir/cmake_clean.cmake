9724e0a8244e374470be296ddbb0bf36b80454c0
liyuanshuo <1405024239@st.nuc.edu.cn>
2018-03-09 17:07:39 +0800

    initial commit

---
+++ 3_4BinaryTreeTravel/cmake-build-debug/CMakeFiles/3_4BinaryTreeTravel.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/3_4BinaryTreeTravel.dir/main.cpp.obj"
+  "3_4BinaryTreeTravel.pdb"
+  "3_4BinaryTreeTravel.exe"
+  "3_4BinaryTreeTravel.exe.manifest"
+  "lib3_4BinaryTreeTravel.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_4BinaryTreeTravel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 3_5BinarySortTree/cmake-build-debug/CMakeFiles/3_5BinarySortTree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/3_5BinarySortTree.dir/main.cpp.obj"
+  "3_5BinarySortTree.pdb"
+  "3_5BinarySortTree.exe"
+  "3_5BinarySortTree.exe.manifest"
+  "lib3_5BinarySortTree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_5BinarySortTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
