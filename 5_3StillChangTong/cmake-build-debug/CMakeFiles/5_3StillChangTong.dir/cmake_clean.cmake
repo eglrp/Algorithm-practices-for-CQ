049420a551b220079155081a539ce0c865333540
liyuanshuo <1405024239@st.nuc.edu.cn>
2018-03-10 17:56:38 +0800

    initial commit

---
+++ 5_3StillChangTong/cmake-build-debug/CMakeFiles/5_3StillChangTong.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/5_3StillChangTong.dir/main.cpp.obj"
+  "5_3StillChangTong.pdb"
+  "5_3StillChangTong.exe"
+  "5_3StillChangTong.exe.manifest"
+  "lib5_3StillChangTong.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/5_3StillChangTong.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
