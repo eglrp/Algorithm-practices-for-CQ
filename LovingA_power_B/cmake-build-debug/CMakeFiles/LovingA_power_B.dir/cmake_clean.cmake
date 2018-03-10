b64a41e42386e6692b4a5fb1518cc393c351e106
liyuanshuo <1405024239@st.nuc.edu.cn>
2018-03-10 10:48:28 +0800

    initial commit

---
+++ LovingA_power_B/cmake-build-debug/CMakeFiles/LovingA_power_B.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LovingA_power_B.dir/main.cpp.obj"
+  "LovingA_power_B.pdb"
+  "LovingA_power_B.exe"
+  "LovingA_power_B.exe.manifest"
+  "libLovingA_power_B.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LovingA_power_B.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
