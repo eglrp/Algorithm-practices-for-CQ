cf98d56ce0934038272919b7793ed0afed91be54
liyuanshuo <1405024239@st.nuc.edu.cn>
2018-03-10 19:33:15 +0800

    initial commit

---
+++ 5_4Freckless/cmake-build-debug/CMakeFiles/5_4Freckless.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/5_4Freckless.dir/main.cpp.obj"
+  "5_4Freckless.pdb"
+  "5_4Freckless.exe"
+  "5_4Freckless.exe.manifest"
+  "lib5_4Freckless.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/5_4Freckless.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
