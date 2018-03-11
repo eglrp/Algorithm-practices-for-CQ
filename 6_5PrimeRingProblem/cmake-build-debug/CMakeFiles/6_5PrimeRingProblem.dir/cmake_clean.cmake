e1c07665dcef951f1c4700e7e650fcf97b4b7774
liyuanshuo <1405024239@st.nuc.edu.cn>
2018-03-11 15:46:24 +0800

    initial commit

---
+++ 6_5PrimeRingProblem/cmake-build-debug/CMakeFiles/6_5PrimeRingProblem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/6_5PrimeRingProblem.dir/main.cpp.obj"
+  "6_5PrimeRingProblem.pdb"
+  "6_5PrimeRingProblem.exe"
+  "6_5PrimeRingProblem.exe.manifest"
+  "lib6_5PrimeRingProblem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/6_5PrimeRingProblem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
