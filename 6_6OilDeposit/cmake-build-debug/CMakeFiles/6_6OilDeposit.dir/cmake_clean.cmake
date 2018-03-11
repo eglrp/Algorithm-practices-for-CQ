658f06f5fba89f44bbaaa78d42d7f2d273d47fb6
liyuanshuo <1405024239@st.nuc.edu.cn>
2018-03-11 16:03:09 +0800

    initial commit

---
+++ 6_6OilDeposit/cmake-build-debug/CMakeFiles/6_6OilDeposit.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/6_6OilDeposit.dir/main.cpp.obj"
+  "6_6OilDeposit.pdb"
+  "6_6OilDeposit.exe"
+  "6_6OilDeposit.exe.manifest"
+  "lib6_6OilDeposit.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/6_6OilDeposit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
