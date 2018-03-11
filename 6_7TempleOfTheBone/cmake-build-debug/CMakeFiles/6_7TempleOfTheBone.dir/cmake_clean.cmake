ef5190e8556744a2f291ed5cdc9069d3e1f2e48f
liyuanshuo <1405024239@st.nuc.edu.cn>
2018-03-11 16:27:06 +0800

    initial commit

---
+++ 6_7TempleOfTheBone/cmake-build-debug/CMakeFiles/6_7TempleOfTheBone.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/6_7TempleOfTheBone.dir/main.cpp.obj"
+  "6_7TempleOfTheBone.pdb"
+  "6_7TempleOfTheBone.exe"
+  "6_7TempleOfTheBone.exe.manifest"
+  "lib6_7TempleOfTheBone.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/6_7TempleOfTheBone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
