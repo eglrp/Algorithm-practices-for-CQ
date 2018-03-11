e178e1940064b560f931e752a0a5b1cbdd2079ad
liyuanshuo <1405024239@st.nuc.edu.cn>
2018-03-11 10:33:59 +0800

    initial commit

---
+++ 6_1HundrandChicken/cmake-build-debug/CMakeFiles/6_1HundrandChicken.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/6_1HundrandChicken.dir/main.cpp.obj"
+  "6_1HundrandChicken.pdb"
+  "6_1HundrandChicken.exe"
+  "6_1HundrandChicken.exe.manifest"
+  "lib6_1HundrandChicken.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/6_1HundrandChicken.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
