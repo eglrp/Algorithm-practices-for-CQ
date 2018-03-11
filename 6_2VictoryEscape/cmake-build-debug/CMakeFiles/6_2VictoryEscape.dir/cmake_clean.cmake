523e36e080b00fc8e7ec32d54fd8d37f5619091c
liyuanshuo <1405024239@st.nuc.edu.cn>
2018-03-11 10:39:10 +0800

    initial commit

---
+++ 6_2VictoryEscape/cmake-build-debug/CMakeFiles/6_2VictoryEscape.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/6_2VictoryEscape.dir/main.cpp.obj"
+  "6_2VictoryEscape.pdb"
+  "6_2VictoryEscape.exe"
+  "6_2VictoryEscape.exe.manifest"
+  "lib6_2VictoryEscape.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/6_2VictoryEscape.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
